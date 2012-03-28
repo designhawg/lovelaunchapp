7c1c1e82a4428e9174f1502f2d38b9183c11133d
designhawg <designhawg@mac.com>
2012-03-27 23:29:46 -0500

    With FaceBook OmniAuth weaved in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovelaunchapp::Application.config.secret_token = '580fddc1ee6dd87cfd8af40c32507fd8d0f1ac28b5d802d9ad269cecc7c0f1ddceefbaf30a330ae9ad98c671db5ef66de3ca2e487c14249a63addb024dfcd27b'
