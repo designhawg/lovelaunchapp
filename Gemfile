b8f6605d34ed3cf0486026795ee09ac085ca167a
designhawg <designhawg@mac.com>
2012-03-28 03:35:07 -0500

    This is the first real build of the UI

--- Gemfile
+++ Gemfile
@@ -5,7 +5,9 @@ gem 'rails', '3.2.2'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+gem "pg", :group => :production
+gem "sqlite3-ruby", :group => :development
+
 gem 'omniauth-facebook'
 
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['206797212760246'], ENV['	b2da9ec0df5d26b233807cbe3e266123']
-  :scope => 'email,offline_access,read_stream,read_friendlists,user_actions.music', :display => 'popup'
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['206797212760246'], ENV[' b2da9ec0df5d26b233807cbe3e266123']
+#   :scope => 'email,offline_access,read_stream,read_friendlists,user_actions.music', :display => 'popup'
+# end
